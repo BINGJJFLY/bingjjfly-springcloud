e943e49ff49b374d53c142af5d0a0bc865fb301f
wangjz <wangjinzhao@lionbridgecapital.cn>
2020-05-29 15:03:56 +0800

    订单支付两个微服务、Eureka集群

---
+++ cloud2020/cloud-consumer-order80/src/main/java/com/wjz/springcloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.wjz.springcloud.controller;
+
+import com.wjz.springcloud.entities.CommonResult;
+import com.wjz.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class OrderController {
+
+    private static final String PAYMENT_URL = "http://127.0.0.1:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value ="/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping(value ="/consumer/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+}
